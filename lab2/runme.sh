96e8f56e1cbc0e7c0a71b4066d9857ffb46c1551
Gustavo Aguiar <gustavo.aguiar@gmail.com>
2018-10-11 01:42:12 -0700

    Adding runme.sh

--- lab2/runme.sh
+++ lab2/runme.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+sleep 60
 yum install httpd -y
 if [ ! -f /tmp/done ]; then
 	curl https://s3.amazonaws.com/sc-publiccloudsummit/lab2/index.html -o /var/www/html/index.html
 	touch /tmp/done
 fi
+service httpd start
+sleep 60
+yum install httpd -y
 service httpd start
 
